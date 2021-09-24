644c1e00db40d7a3402d5b84803b48566c37ddaf
Dharmik Soni <dvrud@Dharmiks-MacBook-Air.local>
2021-09-24 12:56:02 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Featured } from './components/Home/Featured';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Featured />
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home/Featured.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import { Row, Col, Card, Button } from 'react-bootstrap'
+import './Home.css'
+
+const Featured = (props) => {
+	return (
+		<div className='background'>
+			<Row>
+				<img
+					src='/assets/f_name.png'
+					alt=''
+					style={{ marginBottom: '20px', maxWidth: '250px', maxHeight: '150px' }}
+				/>
+			</Row>
+			<Row>
+				{[1, 2].map((card) => (
+					<Col key={card} md={6}>
+						<Card
+							className='text-left'
+							style={{
+								width: '18rem',
+								background: 'white',
+								padding: '2.5px',
+								minWidth: '350px',
+								borderRadius: '2px',
+								border: '1px solid lightgrey'
+							}}
+						>
+							<Card.Img
+								variant='top'
+								src='/assets/feature.png'
+								className='img-fluid'
+								style={{ maxWidth: '350px', maxHeight: '400px' }}
+							/>
+							<Card.Body style={{ paddingLeft: '13px' }}>
+								<Card.Text>
+									<span className='golden-text'>
+										64 1st Avenue, High Street, NZ
+									</span>
+									<h4 style={{ marginTop: '5px' }}>
+										Luxury Avenue Los-Angeles
+									</h4>
+
+									<div
+										style={{ display: 'flex', justifyContent: 'flex-start' }}
+									>
+										<div>
+											<p className='grey-small'>Online Auction</p>
+											<p className='black-medium'>Feb 09 - 23, 2021</p>
+										</div>
+										<div style={{ marginLeft: '15px' }}>
+											<p className='grey-small'>Additional Info</p>
+											<p className='black-medium'>4BD | 2BA | 1,422 sq.ft</p>
+										</div>
+									</div>
+									</Card.Text>
+                                    <hr />
+									<div
+										style={{
+											display: 'flex',
+											justifyContent: 'space-between',
+											alignItems: 'flex-end'
+										}}
+									>
+										<div style={{ alignItems: 'flex-end' }}>
+											<p className='grey-small'>Starting Bid</p>
+											<p className='black-bold'>$256, 000, 000</p>
+										</div>
+										<div
+											style={{
+												alignItems: 'flex-end',
+												display: 'flex',
+												marginRight: '6px'
+											}}
+										>
+											<Button
+												className='black-button text-white'
+												variant='dark'
+											>
+												Place Bid
+											</Button>
+										</div>
+									</div>
+								{/* </Card.Text> */}
+							</Card.Body>
+							<Card.Body></Card.Body>
+						</Card>
+					</Col>
+				))}
+			</Row>
+		</div>
+	)
+}
+
+export { Featured }
