2d3a2a1578f504e975f3e291e599ec470c0330e3
Vi <vienne@labs196.com>
2022-02-02 15:42:24 -0600

    change back to port 5000

--- src/env.js
+++ src/env.js
@@ -1,7 +1,6 @@
 const env = {
-  API_URL: "https://auction10x-env.eba-x6u9pb52.us-east-2.elasticbeanstalk.com",
+  API_URL: "http://localhost:5000",
   API_Key: "AIzaSyAcHd3S5vDRGMkLFIlwKcvCVcA3EetsxMU",
-  IP_API_Key: "38ee1e6a9867a2e1d26a74a8b062ce70fa2b2544592caa97eae190ab",
 };
 
 export default env;
