3aea7634d5a803674662cbc453d8ac509ff6910d
Vi <vienne@labs196.com>
2022-02-02 14:39:41 -0600

    delete /

--- src/env.js
+++ src/env.js
@@ -1,6 +1,5 @@
 const env = {
-  API_URL:
-    "https://auction10x-env.eba-x6u9pb52.us-east-2.elasticbeanstalk.com/",
+  API_URL: "https://auction10x-env.eba-x6u9pb52.us-east-2.elasticbeanstalk.com",
   API_Key: "AIzaSyAcHd3S5vDRGMkLFIlwKcvCVcA3EetsxMU",
   IP_API_Key: "38ee1e6a9867a2e1d26a74a8b062ce70fa2b2544592caa97eae190ab",
 };
