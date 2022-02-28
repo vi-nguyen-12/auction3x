1d34b086b4fcba16e1496ddaf82fe18dde21e87b
Tri Pham <tri.pham@labs196.com>
2022-02-28 11:55:57 -0600

    lmao

---
+++ src/components/Dashboard/Sidebar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { sideBarMenu } from '../Dashboard/SidebarMenu';
+
+function Sidebar() {
+    return (
+        <div>Sidebar</div>
+    )
+}
+
+export default Sidebar
---
+++ src/components/Dashboard/SidebarMenu.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SidebarMenu() {
+    return (
+        <div>SidebarMenu</div>
+    )
+}
+
+export default SidebarMenu
