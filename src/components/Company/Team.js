fcc9b6cef96a48639be5fb35a55351ad748233f0
Tri Pham <tri.pham@labs196.com>
2022-06-06 16:55:24 -0500

    pulllll

--- src/components/Company/Team.js
+++ src/components/Company/Team.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import CompanyHeader from "./CompanyHeader";
 import { Row, Col, Container, Button } from "react-bootstrap";
 import jay from "../../images/jay.jpg";
 import TeamCard from "../Cards/TeamCard";
+import team_member from "../Home/team_data";
 
 function Team({ windowSize }) {
+  const [list, setList] = useState(team_member);
+  const onClick = (title) => () => {
+    if (title === "All") {
+      setList(team_member);
+    }
+    else {
+      setList(team_member.filter(item => item.title === title));
+    }
+  }
+
   return (
     <>
       <CompanyHeader location={"Team"} />
@@ -27,90 +38,132 @@ function Team({ windowSize }) {
             </p>
           </Col>
         </Row>
-        {windowSize > 800 ? (
-          <Row
+
+        <Row
+          style={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignContent: "center",
+          }}
+        >
+          <Col
             style={{
               display: "flex",
               justifyContent: "space-evenly",
               alignContent: "center",
             }}
           >
-            <Col
+            <Button
               style={{
-                display: "flex",
-                justifyContent: "space-evenly",
-                alignContent: "center",
+                fontSize: "20px",
+                background: "none",
+                color: "#706666",
+                fontWeight: "bold",
+                border: "1px solid #e2e2e2 ",
               }}
+              onClick={onClick("All")}
             >
-              <Button
-                style={{
-                  fontSize: "20px",
-                  background: "none",
-                  color: "#706666",
-                  fontWeight: "bold",
-                  border: "1px solid #e2e2e2 ",
-                }}
-              >
-                All
-              </Button>
-              <Button
-                style={{
-                  fontSize: "20px",
-                  background: "none",
-                  color: "#706666",
-                  fontWeight: "bold",
-                  border: "1px solid #e2e2e2 ",
-                }}
-              >
-                Founder
-              </Button>
-              <Button
-                style={{
-                  fontSize: "20px",
-                  background: "none",
-                  color: "#706666",
-                  fontWeight: "bold",
-                  border: "1px solid #e2e2e2 ",
-                }}
-              >
-                Operation
-              </Button>
-              <Button
-                style={{
-                  fontSize: "20px",
-                  background: "none",
-                  color: "#706666",
-                  fontWeight: "bold",
-                  border: "1px solid #e2e2e2 ",
-                }}
-              >
-                Marketing
-              </Button>
-              <Button
-                style={{
-                  fontSize: "20px",
-                  background: "none",
-                  color: "#706666",
-                  fontWeight: "bold",
-                  border: "1px solid #e2e2e2 ",
-                }}
-              >
-                Research
-              </Button>
-              <Button
-                style={{
-                  fontSize: "20px",
-                  background: "none",
-                  color: "#706666",
-                  fontWeight: "bold",
-                  border: "1px solid #e2e2e2 ",
-                }}
-              >
-                Technology
-              </Button>
-            </Col>
-          </Row>
-        ) : (
+              All
+            </Button>
+            <Button
+              style={{
+                fontSize: "20px",
+                background: "none",
+                color: "#706666",
+                fontWeight: "bold",
+                border: "1px solid #e2e2e2 ",
+              }}
+              onClick={onClick("Founder")}
+            >
+              Founder
+            </Button>
+            <Button
+              style={{
+                fontSize: "20px",
+                background: "none",
+                color: "#706666",
+                fontWeight: "bold",
+                border: "1px solid #e2e2e2 ",
+              }}
+              onClick={onClick("Operation")}
+            >
+              Operation
+            </Button>
+            <Button
+              style={{
+                fontSize: "20px",
+                background: "none",
+                color: "#706666",
+                fontWeight: "bold",
+                border: "1px solid #e2e2e2 ",
+              }}
+              onClick={onClick("Research")}
+            >
+              Research
+            </Button>
+            <Button
+              style={{
+                fontSize: "20px",
+                background: "none",
+                color: "#706666",
+                fontWeight: "bold",
+                border: "1px solid #e2e2e2 ",
+              }}
+              onClick={onClick("Marketing")}
+            >
+              Marketing
+            </Button>
+            <Button
+              style={{
+                fontSize: "20px",
+                background: "none",
+                color: "#706666",
+                fontWeight: "bold",
+                border: "1px solid #e2e2e2 ",
+              }}
+              onClick={onClick("Research")}
+            >
+              Research
+            </Button>
+            <Button
+              style={{
+                fontSize: "20px",
+                background: "none",
+                color: "#706666",
+                fontWeight: "bold",
+                border: "1px solid #e2e2e2 ",
+              }}
+              onClick={onClick("Technology")}
+            >
+              Technology
+            </Button>
+            <Button
+              style={{
+                fontSize: "20px",
+                background: "none",
+                color: "#706666",
+                fontWeight: "bold",
+                border: "1px solid #e2e2e2 ",
+              }}
+              onClick={onClick("Business")}
+            >
+              Business
+            </Button>
+            <Button
+              style={{
+                fontSize: "20px",
+                background: "none",
+                color: "#706666",
+                fontWeight: "bold",
+                border: "1px solid #e2e2e2 ",
+              }}
+              onClick={onClick("Legal")}
+            >
+              Legal
+            </Button>
+          </Col>
+        </Row>
+        {/* ) : (
           <Row style={{ padding: "20px 10px" }}>
             <Col
               style={{
@@ -165,16 +218,22 @@ function Team({ windowSize }) {
               </Button>
             </Col>
           </Row>
-        )}
-        <Row >
-          <Col className="ceo-card">
-            <TeamCard
-              name="Jay Patel"
-              location="Sugar Land, Tx, USA"
-              img={jay}
-              linkedln="https://www.linkedin.com/in/wickrema-singhe-66a784/"
-            />
-          </Col>
+        )} */}
+        <Row style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          alignContent: "center",
+        }} >
+          {list.map((member, index) => (
+            <Col className="ceo-card">
+              <TeamCard
+                name={member.name}
+                location={member.location}
+                img={member.img}
+                linkedln={member.linkedin}
+              />
+            </Col>
+          ))}
         </Row>
       </Container>
     </>
